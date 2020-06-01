52d95a996aac2930657e9e8052cee8afa6d83be3
unknown <rushabh.sakharkar1998@gmail.com>
2020-06-02 01:17:45 +0530

    assignment

---
+++ matrix.R
@@ -0,0 +1,42 @@
+makeCatcheMatrix <- function(x = matrix()){
+  inv <- NULL
+  
+  set <- function(y){
+    x <<- y
+    
+    inv <<- NULL
+  }
+  get <- function() {x}
+  
+  setInverse <- function(inverse){inv <<- inverse}
+  
+  getInverse <- function(){inv}
+  
+  list( set = set , get = get , setInverse = setInverse , getInverse = getInverse )
+}
+
+
+catchsolve <- function(x,...){
+  
+  inv <- x$getInverse()
+  
+  if(!is.null(inv)){
+    message("getting catched data")
+    
+    return(inv)
+  }
+  
+  mat <- x$get()
+  
+  inv <- solve(mat,...)
+  
+  x$setInverse(inv)
+  
+  inv
+  
+}
+
+
+
+
+
